a32108040638078cec0946eca0384c96a2ee5093
Jonathan <jondmanning1986@gmail.com>
2025-04-30 16:39:55 -0400

    Write all migrations to src/db/migrations

---
+++ src/db/migrations/20250430161311_create_movies_theaters_table.js
@@ -0,0 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movie_id").unsigned();
+    table.integer("theater_id").unsigned();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+    table
+      .foreign("theater_id")
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+    table.boolean("is_showing");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("movies_theaters");
+};
