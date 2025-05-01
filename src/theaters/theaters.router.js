5d8ee270fb9c20e36f9574cd94d0d77abef50707
Jonathan <jondmanning1986@gmail.com>
2025-05-01 14:55:29 -0400

    Write placeholder routers for movies, reviews, and theaters resources. Complete app.js (enable routers, enable error handlers, enable cors).

---
+++ src/theaters/theaters.router.js
@@ -0,0 +1,5 @@
+const router = require("express").Router({ mergeParams: true });
+const controller = require();
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+module.exports = router;
